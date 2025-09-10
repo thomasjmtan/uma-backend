b875e09acae6e19d8039c3d01a8073c870367ec2
thomasjmtan <180148835+thomasjmtan@users.noreply.github.com>
2025-09-10 22:34:53 +0800

    Complete connection

--- src/main/java/com/tart/Controller/UmaController.java
+++ src/main/java/com/tart/Controller/UmaController.java
@@ -1,11 +1,13 @@
 package com.tart.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/uma")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UmaController {
 
     @GetMapping("/hello")
--- src/main/java/com/tart/UmaMusume_JDBC_Backend/UmaMusumeJdbcBackendApplication.java
+++ src/main/java/com/tart/UmaMusume_JDBC_Backend/UmaMusumeJdbcBackendApplication.java
@@ -4,15 +4,15 @@ import com.tart.Repository.UmaRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.tart")
 public class UmaMusumeJdbcBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UmaMusumeJdbcBackendApplication.class, args);
 
-        UmaRepository newRepo = new UmaRepository();
+        //UmaRepository newRepo = new UmaRepository();
 
-        newRepo.InsertUmamusume("Pay the Butler", 1984, true);
+        //newRepo.InsertUmamusume("Pay the Butler", 1984, true);
 	}
 
 }
